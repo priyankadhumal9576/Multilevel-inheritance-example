a848d233d55dc96e64cfd1883bf260465e5c609f
vishal dhumal <vishal dhumal@LAPTOP-3S3HGFAB>
2022-10-09 12:11:09 +0530

    project is uploaded.

---
+++ src/MainClass.java
@@ -0,0 +1,31 @@
+class A
+{
+String s = "Class A";
+
+}
+class B extends A
+{
+	
+String s = "Class B";
+public B()
+{
+System.out.println(super.s);
+}
+}
+class C extends B
+{
+String s = "Class C";
+public C()
+{
+	
+System.out.println(super.s);
+}
+}
+class MainClass
+{
+public static void main(String[] args)
+{
+C c = new C();
+System.out.println(c.s); 
+} 
+}
